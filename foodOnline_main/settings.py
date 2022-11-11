4ddc7dabcfc454cc781a549a4dc59b2cd3eba458
litansarkar95 <litansarkar95@gmail.com>
2022-11-11 21:53:16 +0600

    accounts, upload profile

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -86,6 +88,8 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'accounts.User'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -126,6 +130,12 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS =[
   'foodOnline_main/static'
 ]
+
+
+# media files configration
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
