f873bee03e2301023fb53dece6714d57d042bbf5
litansarkar95 <litansarkar95@gmail.com>
2022-11-14 23:01:37 +0600

    password hash

--- accounts/views.py
+++ accounts/views.py
@@ -9,11 +9,23 @@ def registerUser(request):
         print(request.POST)
         form =UserForm(request.POST)
         if form.is_valid():
-            password =form.cleaned_data['password']
-            user=form.save(commit=False)
-            user.set_password(password)
+            #password =form.cleaned_data['password']
+            #user=form.save(commit=False)
+            #user.set_password(password)
+            #user.role=User.CUSTOMER
+            #user.save()
+
+
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
             user.role=User.CUSTOMER
             user.save()
+            print('user is create ')
             return redirect('registerUser')
 
     else:
