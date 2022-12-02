fbb7e641ecc2e5d24cd4c6bf83ccc059966db64b
litansarkar95 <litansarkar95@gmail.com>
2022-12-02 08:12:34 +0600

    activate

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,8 @@ from django.contrib import messages,auth
 from . utils import detectUser,send_verification_email
 from django.contrib.auth.decorators import login_required,user_passes_test
 
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
 from django.core.exceptions import PermissionDenied
 
 #Restrict the vendor from accessing the customer page
@@ -118,9 +120,26 @@ def registerVender(request):
 
     return render(request,'accounts/registerVender.html',context)    
 
-
+#activate
 def activate(request,uidb64,token):
-    return
+    try:
+        uid = urlsafe_base64_encode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+
+    except(TypeError,ValueError,OverflowError,User.DoesNotExist):
+        user = None
+
+    if user is not  None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        messages.success(request,"Congratulation ! your account is activated")
+        return redirect('myAccount')
+
+    else:
+        messages.error(request,"Invalid activated link")
+        return redirect('myAccount')    
+
+
+
 
 def login(request):
     if request.user.is_authenticated:
